dd46c17f71b2ea96da659909d813ef9c65a9bfa5
Vishnu <prajapatvishnu049@gmail.com>
2026-06-08 00:21:02 +0530

    Exception and logging completed

---
+++ networksecurity/logging/logger.py
@@ -0,0 +1,22 @@
+'''
+This wil create a logging system for our Python project.
+Logging helps us keep a record of what your program is doing, errors that occur, 
+and debugging information.
+'''
+
+import logging
+import os
+from datetime import datetime
+
+LOG_FILE = f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
+
+logs_path = os.path.join(os.getcwd(),"logs",LOG_FILE)
+os.makedirs(logs_path,exist_ok=True)
+
+LOG_FILE_PATH = os.path.join(logs_path,LOG_FILE)
+
+logging.basicConfig(
+    filename=LOG_FILE_PATH,
+    format = "[ %(asctime)s ] %(lineno)d %(name)s - %(levelname)s - %(message)s",
+    level = logging.INFO,
+)
